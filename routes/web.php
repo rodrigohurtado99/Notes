49a3718992aa0a489ef9d0ac25efdc5269c11f5a
Rodrigo Hurtado <rhcosta99@gmail.com>
2024-09-16 19:12:40 -0300

    001

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Route; // faz referencia a classe routes para conseguimos utilizar, funcionalidades da mesma
+
+// rota que nos retorna a view welcome
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// na rota about, da um echo no texto citado
+Route::get('/about', function(){
+    echo 'About us';
 });
