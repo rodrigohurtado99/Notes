e79461ecebd08cfc42a8ff1d938294160be8c85d
teste <teste@example.com>
2024-09-19 19:07:09 -0300

    006

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+ 
 
 // Controller responsável pelo controle de usuário do site
 class AuthController extends Controller
@@ -10,10 +11,16 @@ class AuthController extends Controller
     public function login(){
 
         return view('login');
-        
+
     }
+
+    public function loginSubmit(Request $request){
+        echo 'Logado';
+    }   
 
     public function logout() {
         echo 'logout';
     }
+
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route; // faz referencia a classe routes para conseguimos utilizar, funcionalidades da mesma
-use App\Http\Controllers\MainController;
 
 // auths routes
 Route::get('/login', [AuthController::class, 'login']);
+// rota para validar login
+Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
 Route::get('/logout', [AuthController::class, 'logout']);
+
