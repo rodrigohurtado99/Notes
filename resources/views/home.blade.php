bb95bb6e2f03350e31899443365793e0233cfa17
teste <teste@example.com>
2024-09-25 21:16:44 -0300

    023

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,6 +8,8 @@
             @include('top_bar') {{-- inclui o top bar que foi criado numa view separada --}}
 
             <!-- no notes available -->
+
+            @if(count($notes) == 0) {{-- se não houver notas ele trás essa div abaixo informando que o usuário não possui notas --}}
             <div class="row mt-5">
                 <div class="col text-center">
                     <p class="display-6 mb-5 text-secondary opacity-50">You have no notes available!</p>
@@ -17,10 +19,9 @@
                 </div>
             </div>
 
-            <!-- temp -->
-            <hr class="my-5">
-
+            @else  {{-- se houver notas ele segue com este fluxo a seguir --}}
             <!-- notes are available -->
+            
             <div class="d-flex justify-content-end mb-3">
                 <a href="{{route('new')}}" class="btn btn-secondary px-3">
                     <i class="fa-regular fa-pen-to-square me-2"></i>New Note
@@ -45,6 +46,8 @@
                     </div>
                 </div>
             </div>
+
+            @endif
         </div>
     </div>
 </div>
