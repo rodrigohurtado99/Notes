5e532bcc3fa1e9175f921bf0123da176a34d4f9f
teste <teste@example.com>
2024-09-25 21:24:23 -0300

    024

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,31 +21,18 @@
 
             @else  {{-- se houver notas ele segue com este fluxo a seguir --}}
             <!-- notes are available -->
-            
+
             <div class="d-flex justify-content-end mb-3">
                 <a href="{{route('new')}}" class="btn btn-secondary px-3">
                     <i class="fa-regular fa-pen-to-square me-2"></i>New Note
                 </a>
             </div>
+            
+            @foreach($notes as $note)
+ 
+            @include('note') {{-- inclui a view notes, que lá possui todas as informações trazidas e layout desta div --}}
 
-            <div class="row">
-                <div class="col">
-                    <div class="card p-4">
-                        <div class="row">
-                            <div class="col">
-                                <h4 class="text-info">Note Title</h4>
-                                <small class="text-secondary"><span class="opacity-75 me-2">Created at:</span><strong>00/00/0000 00:00:00</strong></small>
-                            </div>
-                            <div class="col text-end">
-                                <a href="#" class="btn btn-outline-secondary btn-sm mx-1"><i class="fa-regular fa-pen-to-square"></i></a>
-                                <a href="#" class="btn btn-outline-danger btn-sm mx-1"><i class="fa-regular fa-trash-can"></i></a>
-                            </div>
-                        </div>
-                        <hr>
-                        <p class="text-secondary">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Mollitia temporibus necessitatibus nesciunt quam repellat porro commodi autem veniam doloribus nostrum magni rerum, libero ullam maxime praesentium cum velit. Recusandae, aspernatur.</p>
-                    </div>
-                </div>
-            </div>
+            @endforeach
 
             @endif
         </div>
