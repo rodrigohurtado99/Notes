7e7f68df4a2c4963eecee996eeaf4d64bc8622a4
teste <teste@example.com>
2024-09-30 20:32:58 -0300

    027

---
+++ app/Services/Operations.php
@@ -0,0 +1,24 @@
+<?php
+
+/* Criamos esse arquivo para nos auxiliar com nosso código como a função de encriptar e desencriptar 
+ficou muito repetitiva, criamos essa função que irá ser utilizada em qualquer parte de nosso código
+fazendo com fique mais clean */
+
+namespace App\Services;
+
+use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\Crypt;
+
+class Operations
+{
+    public static function decryptId($value)
+    {
+        try {
+            $value = Crypt::decrypt($value);
+        } catch (DecryptException $e) {
+            return redirect()->route('home');
+        }
+
+        return $value;
+    }
+}
